13472c956b2e4f878739490481f21308c575e6db
Lee Conlin <lee.conlin@distinction.co.uk>
2023-05-26 17:05:47 +0100

    Added API front-end wrapper & abstractions

--- client/src/services/api-abstractions.ts
+++ client/src/services/api-abstractions.ts
@@ -1,42 +1,33 @@
-import {removeStartingSlash, removeTrailingSlash} from "@/utils/urlHelper";
-import {ZodSchema} from "zod";
+import {removeTrailingSlash} from "@/utils/urlHelper";
 
 export const API_BASE_URL = removeTrailingSlash(process.env.API_BASE_URL!);
 
-// TODO: Add API services
-
-interface Api {
+interface ApiAbstractions {
 	baseUrl:string;
 }
 
-export interface FindApi<TFindResult> extends Api {
-	findResultValidator: ZodSchema;
+export interface FindApi<TFindResult> extends ApiAbstractions {
 	findAsync: (id:string) => Promise<TFindResult>;
 }
 
-export interface GetApi<TGetResult> extends Api {
-	getResultValidator: ZodSchema;
+export interface GetApi<TGetResult> extends ApiAbstractions {
 	getAsync: (filter:string, orderBy:string, orderDirection:string) => Promise<TGetResult>;
 }
 
-export interface PostApi<TPostInput, TPostResult> extends Api {
-	postInputValidator: ZodSchema;
-	postResultValidator: ZodSchema;
+export interface PostApi<TPostInput, TPostResult> extends ApiAbstractions {
 	postAsync: (obj:TPostInput) => Promise<TPostResult>;
 }
 
-export interface PutApi<TPutInput, TPutResult> extends Api {
-	putInputValidator: ZodSchema;
-	putResultValidator: ZodSchema;
+export interface PutApi<TPutInput, TPutResult> extends ApiAbstractions {
 	putAsync: (obj:TPutInput) => Promise<TPutResult>;
 }
 
-export interface PatchApi<TPatchResult> extends Api {
-	patchResultValidator: ZodSchema;
+export interface PatchApi<TPatchResult> extends ApiAbstractions {
 	patchAsync: (patch:JsonPatch) => Promise<TPatchResult>;
 }
 
-export interface DeleteApi<TDeleteResult> extends Api {
-	deleteResultValidator: ZodSchema;
+export interface DeleteApi<TDeleteResult> extends ApiAbstractions {
 	deleteAsync: (id:string) => Promise<TDeleteResult>;
 }
+
+// TODO define zod and types for ValidationProblem and Problem
---
+++ client/src/services/exampleApi.ts
@@ -0,0 +1,49 @@
+import {API_BASE_URL, FindApi, PostApi} from "@/services/api-abstractions";
+import {z, ZodSchema} from "zod";
+
+export class ExampleApi	implements
+	FindApi<ExampleObj>,
+	PostApi<CreateExampleObj, ExampleObj>
+{
+	baseUrl: string;
+
+	constructor(getResultValidator:ZodSchema, postInputValidator:ZodSchema, postResultValidator:ZodSchema) {
+		this.baseUrl = `${API_BASE_URL}/v1/example`
+	}
+
+	async findAsync(id: string): Promise<ExampleObj> {
+		const response = await fetch(`${this.baseUrl}/${id}`);
+		if(response.status !== 200){ // Ok
+			// handle error here
+		}
+		return await ExampleObjSchema.parseAsync(await response.json());
+	}
+
+	async postAsync(obj: CreateExampleObj): Promise<ExampleObj> {
+		const model = await CreateExampleObjSchema.parseAsync(obj);
+
+		const response = await fetch(this.baseUrl, {
+			method: "POST",
+			body: JSON.stringify(model)
+		})
+
+		if(response.status !== 201){ // created
+			// handle error here
+		}
+
+		return await ExampleObjSchema.parseAsync(await response.json());
+	}
+
+}
+
+// TODO define zod & type for ExampleEntity
+const ExampleObjSchema = z.object({
+	id: z.string(),
+	name: z.string()
+});
+export type ExampleObj = z.infer<typeof ExampleObjSchema>;
+
+const CreateExampleObjSchema = z.object({
+	name: z.string()
+});
+export type CreateExampleObj = z.infer<typeof CreateExampleObjSchema>;
