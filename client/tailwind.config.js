eb4ff6ecb03e8bd06aa5652c892666cdfb5ae5c6
Liam Styles <liam.styles@distinction.co.uk>
2023-06-27 16:43:50 +0100

    tailwind config setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,7 +14,24 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        branding: {
+          "primary": "#000000",
+          "secondary": "#FFFFFF"
+        },
+        status: {
+          "error": "#FFF000",
+          "success": "#218A07",
+          "warning": "#C7C10A",
+          "neutral": "#F5F5F5"
+        }
+      },
+      fontFamily: {
+
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/container-queries")
+  ],
 }
